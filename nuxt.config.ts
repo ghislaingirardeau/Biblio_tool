a5c7286503ee6bf36dd9c236515c003fca0c2f97
ghislaingirardeau <g.girardeaupro@sfr.fr>
2025-07-11 14:45:21 +0700

    init: vue use & nuxt ui

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,5 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-05-15',
   devtools: { enabled: true },
-  modules: ['@nuxt/eslint', '@nuxt/icon', '@nuxt/ui']
-})
+  modules: ['@nuxt/eslint', '@nuxt/icon', '@nuxt/ui', '@vueuse/nuxt'],
+});
